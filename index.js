1aec116f221c1c8d71dfa37121a6bb4fc499138d
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-08-10 12:29:01 +0530

    connected to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 import express from "express";
 import Routes from "./src/Routes/crmRoute";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
+
+// mongodb connection
+
+mongoose.connect(
+  "mongodb+srv://crmuser:AoGNtmbhseBDSLHH@cluster0.lh17j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (err) {
+      console.log("Error connecting to DB");
+    } else {
+      console.log("Success connecting to DB");
+    }
+  }
+);
+
+// body-parser setup
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 Routes(app);
 
--- src/Routes/crmRoute.js
+++ src/Routes/crmRoute.js
@@ -2,6 +2,7 @@ const Routes = (app) => {
   app
     .route("/contact")
     .get(
+      // middleware
       (req, res, next) => {
         console.log(`Request from ${req.originalUrl}`);
         console.log(`Request type ${req.method}`);
